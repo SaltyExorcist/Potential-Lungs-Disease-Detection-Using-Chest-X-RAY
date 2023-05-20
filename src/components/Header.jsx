99b0333c75c38faf64ae35aa3c96303625355d70
SaltyExorcist <57772990+SaltyExorcist@users.noreply.github.com>
2023-05-21 01:33:56 +0530

    Added the loading animation.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,6 +6,7 @@ import Logo from './logo.png'
 function Header() {
   const [file, setFile] = useState(null);
   const [result, setResult] = useState(null);
+  const [showCard, setShowCard] = useState(false);
 
   const handleFileChange = (event) => {
     setFile(event.target.files[0]);
@@ -27,6 +28,11 @@ function Header() {
     setResult(response.data.result);
   };
 
+  const handleContactClick = () => {
+    event.preventDefault();
+    setShowCard(true);
+  };
+
   return (
     <header>
       <div className="logo">
@@ -36,8 +42,16 @@ function Header() {
 			<li><a href="/" className="active">Home</a></li>
 			<li><a href="/About">About us</a></li>
 			<li><a href="https://github.com/SaltyExorcist/Potential-Lungs-Disease-Detection-Using-Chest-X-RAY" target="_blank" rel="noopener noreferrer">Contribute</a></li>
-			<li><a href="/">Contact us</a></li>
+			<li><a href="/" onClick={handleContactClick}>Contact us</a></li>
 		</ul>
+    {showCard && (
+        <div className="card">
+          <h3>Contact Information</h3>
+          <p>Email: info@example.com</p>
+          <p>Phone: 123-456-7890</p>
+          {/* Additional content of the card */}
+        </div>
+      )}
 	</header>
   );
 }
